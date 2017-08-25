0bc666f625d4eb2f03155321da5850c52e930b88
Eduard Carreras <ecarreras@gisce.net>
2017-08-25 11:09:18 +0200

    Bump to v1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='apply_pr',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
