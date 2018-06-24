25734c31f8beb6f2177fe5abcaa4f86418f9b63b
Gerard Dalmau <gddalmau@gmail.com>
2018-06-24 15:01:38 +0200

    Add sastre and tailor cli options for apply_pr

--- apply_pr/cli.py
+++ apply_pr/cli.py
@@ -4,9 +4,35 @@ from urlparse import urlparse
 
 from fabric.tasks import execute, WrappedCallableTask
 from fabric.api import env
+from fabric import colors
 import click
 
 DEFAULT_LOG_LEVEL = 'ERROR'
+
+github_options = [
+    click.option('--owner', help='GitHub owner name', default='gisce', show_default=True),
+    click.option('--repository', help='GitHub repository name', default='erp', show_default=True),
+
+]
+
+deployment_options = github_options + [
+    click.option("--host", help="Host to apply", required=True),
+    click.option('--src', help='Remote src path',  default='/home/erp/src', show_default=True),
+]
+
+apply_pr_options = deployment_options + [
+    click.option("--pr", help="Pull request to apply", required=True),
+    click.option("--from-number", help="From commit number", default=0),
+    click.option("--from-commit", help="From commit hash (included)", default=None),
+    click.option("--force-hostname", help="Force hostname",  default=False)
+]
+
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _add_options
 
 
 def configure_logging():
@@ -16,21 +42,9 @@ def configure_logging():
     logging.basicConfig(level=log_level)
 
 
-@click.command(name="apply_pr")
-@click.option("--pr", help="Pull request to apply", required=True)
-@click.option("--host", help="Host to apply", required=True)
-@click.option("--from-number", help="From commit number", default=0)
-@click.option("--from-commit", help="From commit hash (included)",
-              default=None, show_default=True)
-@click.option("--force-hostname", help="Force hostname",
-              default=False, show_default=True)
-@click.option('--owner', help='GitHub owner name',
-              default='gisce', show_default=True)
-@click.option('--repository', help='GitHub repository name',
-              default='erp', show_default=True)
-@click.option('--src', help='Remote src path',
-              default='/home/erp/src', show_default=True)
-def apply_pr(
+@click.command(name="sastre")
+@add_options(apply_pr_options)
+def sastre(
         pr, host, from_number, from_commit, force_hostname,
         owner, repository, src
 ):
@@ -52,16 +66,26 @@ def apply_pr(
         host=url.hostname
     )
 
+@click.command()
+@add_options(apply_pr_options)
+def tailor(**kwargs):
+    sastre()
+
+@click.command()
+@add_options(apply_pr_options)
+def apply_pr(**kwargs):
+    """You can apply patches using 'sastre' or 'tailor'.\n
+    Use 'sastre --help' or 'tailor --help' for more information."""
+    print(colors.yellow(
+        "You can apply patches using 'sastre' or 'tailor'.\n"
+        "Use 'sastre --help' or 'tailor --help' for more information."
+    ))
+    sastre()
+
 
 @click.command(name='check_pr')
 @click.option('--pr', help='Pull request to check', required=True)
-@click.option('--host', help='Host to check', required=True)
-@click.option('--owner', help='GitHub owner name',
-              default='gisce', show_default=True)
-@click.option('--repository', help='GitHub repository name',
-              default='erp', show_default=True)
-@click.option('--src', help='Remote src path',
-              default='/home/erp/src', show_default=True)
+@add_options(deployment_options)
 def check_pr(pr, src, owner, repository, host):
     from apply_pr import fabfile
 
@@ -80,10 +104,7 @@ def check_pr(pr, src, owner, repository, host):
 @click.option('--deploy-id', help='Deploy id to mark')
 @click.option('--status', type=click.Choice(['success', 'error', 'failure']),
               help='Status to set', default='success', show_default=True)
-@click.option('--owner', help='GitHub owner name',
-              default='gisce', show_default=True)
-@click.option('--repository', help='GitHub repository name',
-              default='erp', show_default=True)
+@add_options(github_options)
 def status_pr(deploy_id, status, owner, repository):
     from apply_pr import fabfile
 
@@ -102,10 +123,7 @@ def status_pr(deploy_id, status, owner, repository):
               default=' ', required=True, show_default=True)
 @click.option('--version',
               help="Compare with milestone and show if included in prs")
-@click.option('--owner', help='GitHub owner name',
-              default='gisce', show_default=True)
-@click.option('--repository', help='GitHub repository name',
-              default='erp', show_default=True)
+@add_options(github_options)
 def check_prs_status(prs, separator, version, owner, repository):
     from apply_pr import fabfile
 
@@ -120,17 +138,14 @@ def check_prs_status(prs, separator, version, owner, repository):
             version=version)
 
 
-@click.command(name='check_prs_status')
+@click.command(name='create_changelog')
 @click.option('-m', '--milestone', required=True,
               help='Milestone to get the issues from (version)')
 @click.option('--issues/--no-issues', default=False, show_default=True,
               help='Also get the data on the issues')
 @click.option('--changelog_path', default='/tmp', show_default=True,
               help='Path to drop the changelog file in')
-@click.option('--owner', help='GitHub owner name',
-              default='gisce', show_default=True)
-@click.option('--repository', help='GitHub repository name',
-              default='erp', show_default=True)
+@add_options(github_options)
 def create_changelog(milestone, issues, changelog_path, owner, repository):
     from apply_pr import fabfile
 
@@ -146,4 +161,4 @@ def create_changelog(milestone, issues, changelog_path, owner, repository):
 
 
 if __name__ == '__main__':
-    apply_pr()
+    sastre()
--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ setup(
     description='Apply Pull Requests from GitHub',
     entry_points='''
         [console_scripts]
+        sastre=apply_pr.cli:sastre
+        tailor=apply_pr.cli:tailor
         apply_pr=apply_pr.cli:apply_pr
         check_pr=apply_pr.cli:check_pr
         status_pr=apply_pr.cli:status_pr
