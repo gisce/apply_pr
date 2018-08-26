e4af7eeb714cb06d94f760c77008336ce0bfa947
Eduard Carreras <ecarreras@gisce.net>
2018-08-26 10:11:45 +0200

    Bump to v1.8.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='apply_pr',
-    version='1.8.1',
+    version='1.8.2',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
