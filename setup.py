e035b3ebd3e44ebffd52b6250f0c59dc6f434f08
Jaume Florez <jflorez@gisce.net>
2019-01-18 11:20:09 +0100

    Bump to v2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='apply_pr',
-    version='2.0.0',
+    version='2.1.0',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
