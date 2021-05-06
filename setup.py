e66dde7400701f882aaf0d1f07894007e179e032
Eduard Carreras <ecarreras@gisce.net>
2021-05-06 21:54:03 +0200

    Bump to v2.10.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='apply_pr',
-    version='2.10.0',
+    version='2.10.1',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
