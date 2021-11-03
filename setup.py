feaa4bbc206b244128bf1ec9c675c3d6d79d96df
david <dmunoz@gisce.net>
2021-11-03 09:51:20 +0100

    Bump to v2.11.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='apply_pr',
-    version='2.11.1',
+    version='2.11.2',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
