59aaf215969a1471ccbe6bb4205403ef092f405f
giscegit <devel@gisce.net>
2023-02-06 11:37:34 +0000

    Bump to v2.17.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='apply_pr',
-    version='2.16.0',
+    version='2.17.0',
     packages=find_packages(),
     url='https://github.com/gisce/apply_pr',
     license='MIT',
